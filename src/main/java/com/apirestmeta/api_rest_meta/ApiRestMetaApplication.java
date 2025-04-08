d3eb568307db08ebd31ebfd6347b5d2cfa029f00
Roberto <roberto@gmail.com>
2025-04-08 00:18:08 -0600

    primer commit. Creando proyecto

---
+++ src/main/java/com/apirestmeta/api_rest_meta/ApiRestMetaApplication.java
@@ -0,0 +1,13 @@
+package com.apirestmeta.api_rest_meta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestMetaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestMetaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/apirestmeta/api_rest_meta/ApiRestMetaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apirestmeta.api_rest_meta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestMetaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
