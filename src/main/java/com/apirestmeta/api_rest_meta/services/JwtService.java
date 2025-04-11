f9e691dbc543e69dc7108b25a110ec968702d9f5
ALEJANDRA BAUTISTA CELESTINO <alejandra.bautista@ddvcmx.onmicrosoft.com>
2025-04-11 12:03:21 -0600

    poniendo una llave fija al crear el token

--- src/main/java/com/apirestmeta/api_rest_meta/services/JwtService.java
+++ src/main/java/com/apirestmeta/api_rest_meta/services/JwtService.java
@@ -1,24 +1,34 @@
 package com.apirestmeta.api_rest_meta.services;
 
+import java.nio.charset.StandardCharsets;
 import java.security.Key;
 import java.sql.Date;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
+import jakarta.annotation.PostConstruct;
 
 @Service
 public class JwtService {
-    private final Key key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+    @Value("${jwt.secret}")
+    private String secret;
+
+    private Key key;
+    @PostConstruct
+    public void init() {
+        key = Keys.hmacShaKeyFor(secret.getBytes(StandardCharsets.UTF_8));
+    }
 
     public String generateToken(String username) {
         return Jwts.builder()
                 .setSubject(username)
                 .setIssuedAt(new java.util.Date())
                 .setExpiration(new Date(System.currentTimeMillis() + 3600000)) 
-                .signWith(key)
+                .signWith(key, SignatureAlgorithm.HS256)
                 .compact();
     }
 
