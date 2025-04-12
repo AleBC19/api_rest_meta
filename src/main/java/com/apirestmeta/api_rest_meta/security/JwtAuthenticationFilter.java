a5752ee5093fef9e80f1f62445d71848ab926509
ALEJANDRA BAUTISTA CELESTINO <alejandra.bautista@ddvcmx.onmicrosoft.com>
2025-04-11 18:16:13 -0600

    ultimos cambios en autenticacion

--- src/main/java/com/apirestmeta/api_rest_meta/security/JwtAuthenticationFilter.java
+++ src/main/java/com/apirestmeta/api_rest_meta/security/JwtAuthenticationFilter.java
@@ -46,7 +46,6 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter  {
 
             SecurityContextHolder.getContext().setAuthentication(authToken);
         }
-
         filterChain.doFilter(request, response);
     }
 }
